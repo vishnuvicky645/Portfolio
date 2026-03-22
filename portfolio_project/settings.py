ce0498053ae0c06cc50b62cf8f7c9967ea516adb
vishnuvardhanreddy <vishnuvardhanreddy260@gmail.com>
2026-03-23 00:51:13 +0530

    Fix media files for Django deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -139,8 +139,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
